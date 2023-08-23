34ce49da69c04ece8c4fa52482818185d2b3ed8f
jorginho222 <rodizioperalta22@gmail.com>
2023-08-23 17:32:20 -0300

    Use effect, use ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import {Button} from "./components/Button";
 import Alert from "./components/Alert";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
     const [alertVisible, setAlertVisible] = useState(false)
+    let [counter, setCounter] = useState(0)
+    const increment = () => setCounter(counter => counter + 1)
+    useEffect(() => {
+        console.log(alertVisible)
+    }, [alertVisible])
+
     return <>
         { alertVisible &&
           <Alert onClose={() => setAlertVisible(false)}>My alert</Alert> }
@@ -13,6 +19,8 @@ function App() {
         >
             My button
         </Button>
+        <button onClick={increment}>Aum</button>
+        {counter}
     </>
 }
 export default App
